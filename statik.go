5abb5b88292cd6ce950e3184877b0b550f1dc3e3
VaiTon <eyadlorenzo@gmail.com>
2023-07-07 14:59:06 +0200

    Use go casing standards

--- statik.go
+++ statik.go
@@ -349,21 +349,21 @@ func walk(base string) (dir Directory, fz []FuzzyFile, err error) {
 func copyFile(f FuzzyFile) (err error) {
 
 	// Open the input file
-	input_stream, err := os.Open(f.SrcPath)
+	inputStream, err := os.Open(f.SrcPath)
 	if err != nil {
 		return fmt.Errorf("could not open %s for reading:\n%s", f.SrcPath, err)
 	}
-	defer input_stream.Close()
+	defer inputStream.Close()
 
 	// Create the output file, truncating it if it already exists
-	output_stream, err := os.Create(f.DstPath)
+	outputStream, err := os.Create(f.DstPath)
 	if err != nil {
 		return fmt.Errorf("could not open %s for writing:\n%s", f.DstPath, err)
 	}
-	defer output_stream.Close()
+	defer outputStream.Close()
 
 	// Copy the file by using io.Copy(), which handles large files efficiently
-	if _, err := io.Copy(output_stream, input_stream); err != nil {
+	if _, err := io.Copy(outputStream, inputStream); err != nil {
 		return fmt.Errorf("error while copying %s to %s:\n%s", f.SrcPath, f.DstPath, err)
 	}
 
